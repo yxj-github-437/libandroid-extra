18d2ad6c19cdd5001ed630fba7ced78599ae66a5
yxj-github-437 <2457369732@qq.com>
2025-06-04 03:09:48 +0800

    implement wctype.h function towctrans{_l} and wctrans{_l}

---
+++ libc-extra/src/wctype.c
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+#include <wctype.h>
+
+#define wctrans_tolower (wctrans_t)1
+#define wctrans_toupper (wctrans_t)2
+
+wint_t towctrans(wint_t c, wctrans_t t) {
+    if (t == wctrans_tolower) towlower(c);
+    if (t == wctrans_toupper) towupper(c);
+    errno = EINVAL;
+    return 0;
+}
+
+wctrans_t wctrans(const char* name) {
+    if (strcmp(name, "tolower") == 0) return wctrans_tolower;
+    if (strcmp(name, "toupper") == 0) return wctrans_toupper;
+    return 0;
+}
+
+wint_t towctrans_l(wint_t c, wctrans_t t, locale_t) {
+    return towctrans(c, t);
+}
+
+wctrans_t wctrans_l(const char* name, locale_t) {
+    return wctrans(name);
+}
